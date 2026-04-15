cdcb5f5ce155cb787ca14ec6a42e7de5769c1b08
villaverdemaxi09-gif <villaverdemaxi09@gmail.com>
2026-04-14 21:10:46 -0300

    Ejercicio 1: variables y operaciones

---
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,11 @@
+const nombre = "Maximo";
+const edad = 21;
+const materia = "Desarrollo de Software";
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+let contador = 0;
+
+contador = contador + 1;
+contador = contador + 1;
+contador = contador + 1;
+
+console.log("Valor final:", contador);
