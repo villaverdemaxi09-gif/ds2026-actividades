c80fffa9633b2d410de5572c870b164bbf7d0683
villaverdemaxi09-gif <villaverdemaxi09@gmail.com>
2026-06-01 09:27:33 -0300

    feat: agregar Layout - hito 2

---
+++ ejercicios/c09-libreria-react-layout/src/components/Layout/Header.tsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <nav className="navbar navbar-dark bg-dark px-4">
+      <span className="navbar-brand fw-bold fs-4">LibreriaDS</span>
+      <div className="d-flex gap-3">
+        <a href="#" className="text-white text-decoration-none">Inicio</a>
+        <a href="#" className="text-white text-decoration-none">Catalogo</a>
+        <a href="#" className="text-white text-decoration-none">Contacto</a>
+      </div>
+    </nav>
+
+)
+}
+
+export default Header
